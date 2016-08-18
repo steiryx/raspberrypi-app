c60bf4b0a71a2b2dafbc8025b00ac3289044ee59
steiryx <steiryx@yahoo.com.ph>
2016-08-18 21:58:58 +0800

    Revert "Revert "Getting started with picamera""
    This reverts commit d4d8fc3b2b5ff2d63160b398f675e0629be22088.

---
+++ still pictures.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+#It's important to sleep for at least 2 seconds before capturing,
+#to give the sensor time to set its light levels.
+sleep(5)
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+#adding a loop to take five pictures in a row:
+camera.start_preview()
+for i in range(5):
+    sleep(5)
+    #The variable i contains the current iteration number, from 0 to 4,
+    #so the images will be saved as image0.jpg, image1.jpg and so on.
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+camera.stop_preview()
