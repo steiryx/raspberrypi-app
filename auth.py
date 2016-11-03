855202f9fbd24cf7d69291dbbb4a99ec6ee6492d
steiryx <steiryx@yahoo.com.ph>
2016-11-03 23:15:16 +0800

    twitter
    twitter apps

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key		=	"ix0MwxEtMaGmMjWUIj56NZYaf"
+consumer_secret		=	"Y4fVFipPDdNnFZJ8aZO2UUvMIX7T4tjodZdwgQHokBJy0CKQah"
+access_token		=	"794179137275445248-7iHTl0ZKobqCTNmAZPKgbr1oLQXQXah"
+access_token_secret	=	"PfP8C2PYhuHWGZady56cXKbNopDylRcQLvI8mNJcIU8Eu"
